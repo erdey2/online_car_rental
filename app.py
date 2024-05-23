378cfab6d720a62a5a663eb2cb547866ae2376d0
Erdey Syoum <erdeysyoum@gmail.com>
2024-05-23 15:40:52 +0300

    app.py added

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from models import db, User, Car, Rental
+from flask_login import LoginManager
+
 app = Flask(__name__)
+app.config.from_object('config_Config')
+
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-@app.route('/')
-def home():
-    return 'Online Car Rental Management System'
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
