50bb906ad6590b5372822b47139b4be85c28282d
Erdey <erdey.syoum@gmail.com>
2024-06-05 18:47:32 +0300

    environment variable added

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///car_rental.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(24))
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///car_rental.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
