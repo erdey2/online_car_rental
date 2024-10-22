e6e48ce350bf1ccb5762152eb5c67db1147c269c
erdey2 <erdeysyoum@gmail.com>
2024-10-22 12:52:17 +0000

    run.py updated

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
-from app import app
-from app.models import db
-from app import routes
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///car_rental.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# Setup Flask-Migrate
+migrate = Migrate(app, db)
+
+from app import routes  # Ensure your routes are imported
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
-    # app.run(host='0.0.0.0', port=8000, debug=True)
+
+
